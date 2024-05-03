260b9ed6a8f15e645b8d36ec976e403deda7e7c3
Chr1stmasBread <93283740+Chr1stmasBread@users.noreply.github.com>
2024-05-03 11:37:24 +0300

    Update GPT.py

--- GPT.py
+++ GPT.py
@@ -22,116 +22,126 @@ resource_limits = {
 }
 
 # Функция для отправки текста в Yandex GPT и получения ответа
-@bot.message_handler(commands=['generate'])
 def generate_text(query):
-		headers = {
-			'Authorization': f'Bearer {IAM_TOKEN}',
-			'Content-Type': 'application/json'
-		}
-		data = {
-			"modelUri": f"gpt://{FOLDER_ID}/yandexgpt/latest",
-			"completionOptions": {
-				"stream": False,
-				"temperature": 0.8,
-				"maxTokens": "2000"
-			},
-			"messages": [
-				{
-					"role": "user",
-					"text": query
-				}
-			]
-		}
+    try:
+        headers = {
+            'Authorization': f'Bearer {IAM_TOKEN}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "modelUri": f"gpt://{FOLDER_ID}/yandexgpt/latest",
+            "completionOptions": {
+                "stream": False,
+                "temperature": 0.8,
+                "maxTokens": "2000"
+            },
+            "messages": [
+                {
+                    "role": "user",
+                    "text": query
+                }
+            ]
+        }
 
-		response = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
-								 headers=headers,
-								 json=data)
+        response = requests.post("https://llm.api.cloud.yandex.net/foundationModels/v1/completion",
+                                 headers=headers,
+                                 json=data)
 
-		if response.status_code == 200:
-			text = response.json()["result"]["alternatives"][0]["message"]["text"]
-			return text
-		else:
-			error_message = 'Invalid response received: code: {}, message: {}'.format(response.status_code,
-																					  response.text)
-			return error_message
+        if response.status_code == 200:
+            text = response.json()["result"]["alternatives"][0]["message"]["text"]
+            return text
+        else:
+            error_message = 'Invalid response received: code: {}, message: {}'.format(response.status_code,
+                                                                                      response.text)
+            return error_message
+    except Exception as e:
+        return str(e)
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
-		bot.reply_to(message, 'Привет! Для начала работы введите /tts, если Вы хотите перевести текст в аудио формат. Либо /generate, чтобы начать работу с обычным GPT.')
-# Обработчик текстовых сообщений
-@bot.message_handler(func=lambda message: True, commands = ['generate'])
-def handle_text(message):
-		query = message.text
-		generated_text = generate_text(query)
-		bot.reply_to(message, generated_text)
+    bot.reply_to(message, 'Привет! Для начала работы введите /tts, если Вы хотите перевести текст в аудио формат. Либо /generate, чтобы начать работу с обычным GPT.')
 
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True, commands=['generate'])
+def handle_text(message):
+    query = message.text
+    generated_text = generate_text(query)
+    bot.reply_to(message, generated_text)
 
 # Функция для преобразования голосового сообщения в текст с помощью SpeechKit
 @bot.message_handler(content_types=['voice'])
 def voice_to_text(message):
-	# Получаем информацию о голосовом сообщении
-	file_info = bot.get_file(message.voice.file_id)
-	file_path = file_info.file_path
+    try:
+        # Получаем информацию о голосовом сообщении
+        file_info = bot.get_file(message.voice.file_id)
+        file_path = file_info.file_path
 
-	# Скачиваем аудиофайл
-	file_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
-	audio_file = requests.get(file_url)
+        # Скачиваем аудиофайл
+        file_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+        audio_file = requests.get(file_url)
 
-	# Отправляем "набирает сообщение" в чат
-	bot.send_chat_action(message.chat.id, 'typing')
+        # Отправляем "набирает сообщение" в чат
+        bot.send_chat_action(message.chat.id, 'typing')
 
-	# Параметры запроса к SpeechKit
-	headers = {'Authorization': f'Bearer {IAM_TOKEN}'}
-	files = {'file': audio_file.content}
-	params = {'folderId': FOLDER_ID, 'lang': 'ru-RU'}  # Язык текста (русский)
+        # Параметры запроса к SpeechKit
+        headers = {'Authorization': f'Bearer {IAM_TOKEN}'}
+        files = {'file': audio_file.content}
+        params = {'folderId': FOLDER_ID, 'lang': 'ru-RU'}  # Язык текста (русский)
 
-	# Отправка запроса на распознавание речи
-	response = requests.post('https://stt.api.cloud.yandex.net/speech/v1/stt:recognize', headers=headers, files=files,
-							 params=params)
+        # Отправка запроса на распознавание речи
+        response = requests.post('https://stt.api.cloud.yandex.net/speech/v1/stt:recognize', headers=headers,
+                                 files=files,
+                                 params=params)
 
-	# Обработка ответа
-	if response.status_code == 200:
-		text = response.json()['result']
-		bot.reply_to(message, f"Распознанный текст:\n{text}")
-	else:
-		bot.reply_to(message, "Ошибка распознавания аудио")
+        # Обработка ответа
+        if response.status_code == 200:
+            text = response.json()['result']
+            bot.reply_to(message, f"Распознанный текст:\n{text}")
+        else:
+            bot.reply_to(message, "Ошибка распознавания аудио")
+    except Exception as e:
+        bot.reply_to(message, f"Ошибка при обработке голосового сообщения: {str(e)}")
 
 # Функция для преобразования текста в голос с помощью SpeechKit
 @bot.message_handler(commands=['tts'])
 def request_text(message):
-		bot.send_message(message.chat.id, "Я готов к работе с вашим текстом. Пожалуйста, отправьте текст для озвучки.")
-		bot.register_next_step_handler(text_to_speech)
-	# Функция для обработки текстовых сообщений после команды /tts
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+    bot.send_message(message.chat.id, "Я готов к работе с вашим текстом. Пожалуйста, отправьте текст для озвучки.")
+    bot.register_next_step_handler(message, text_to_speech)
+
+# Функция для обработки текстовых сообщений после команды /tts
 def text_to_speech(message):
-		# Отправляем "набирает сообщение" в чат
-		bot.send_chat_action(message.chat.id, 'typing')
+    try:
+        # Отправляем "набирает сообщение" в чат
+        bot.send_chat_action(message.chat.id, 'typing')
 
-		# Получаем текст из сообщения пользователя
-		text = message.text
+        # Получаем текст из сообщения пользователя
+        text = message.text
 
-		# Параметры запроса к SpeechKit
-		data = {
-			'text': text,
-			'speed': 1.1,  # Скорость речи
-			'emotion': 'good',  # Эмоциональная окраска
-			'lang': 'ru-RU',  # Язык текста (русский)
-			'voice': 'jane',  # Голос Джейн
-			'folderId': FOLDER_ID,
-		}
-		headers = {'Authorization': f'Bearer {IAM_TOKEN}'}
+        # Параметры запроса к SpeechKit
+        data = {
+            'text': text,
+            'speed': 1.1,  # Скорость речи
+            'emotion': 'good',  # Эмоциональная окраска
+            'lang': 'ru-RU',  # Язык текста (русский)
+            'voice': 'jane',  # Голос Джейн
+            'folderId': FOLDER_ID,
+        }
+        headers = {'Authorization': f'Bearer {IAM_TOKEN}'}
 
-		# Отправка запроса к SpeechKit
-		response = requests.post('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize', headers=headers,
-								 data=data)
+        # Отправка запроса к SpeechKit
+        response = requests.post('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize', headers=headers,
+                                 data=data)
 
-		# Обработка ответа
-		if response.status_code == 200:
-			# Отправка аудиосообщения пользователю
-			bot.send_voice(message.chat.id, response.content)
-		else:
-			# Сообщение об ошибке, если запрос к SpeechKit завершился неудачно
-			bot.reply_to(message, "При преобразовании текста в речь возникла ошибка")
+        # Обработка ответа
+        if response.status_code == 200:
+            # Отправка аудиосообщения пользователю
+            bot.send_voice(message.chat.id, response.content)
+        else:
+            # Сообщение об ошибке, если запрос к SpeechKit завершился неудачно
+            bot.reply_to(message, "При преобразовании текста в речь возникла ошибка")
+    except Exception as e:
+        bot.reply_to(message, f"Ошибка при преобразовании текста в речь: {str(e)}")
+
 # Запуск бота
 bot.polling()
